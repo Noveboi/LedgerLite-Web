eed71c1ec2450de49167d3956f41620dd04ab61b
Nove <cptgiorgos03@gmail.com>
2025-05-13 15:21:37 +0300

    Add LoginForm

--- ledger-lite/src/app/features/login/components/login-form/login-form.component.ts
+++ ledger-lite/src/app/features/login/components/login-form/login-form.component.ts
@@ -1,14 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { LoginRequest } from '../../types';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { merge } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-login-form',
-  imports: [MatFormField, MatInputModule, MatButtonModule],
+  imports: [MatFormField, MatInputModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  })
 
+  emailValidation = signal('')
+  passwordValidation = signal('')
+  onSubmit = output<LoginRequest>()
+
+  constructor() {
+    merge(this.loginForm.statusChanges, this.loginForm.valueChanges)
+    .pipe(takeUntilDestroyed())
+    .subscribe(() => this.setValidationErrors())
+  }
+
+  private setValidationErrors() {
+    const controls = this.loginForm.controls;
+
+    if (controls.email.hasError('email')) {
+      this.emailValidation.set('Email is not a valid address')
+    } else if (controls.email.hasError('required')) {
+      this.emailValidation.set('You must enter a value')
+    } 
+
+    if (controls.password.hasError('required')) {
+      this.passwordValidation.set('You must enter a value')
+    }
+  }
+
+  onFormSubmit(e: SubmitEvent) {
+    e.preventDefault()
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const credentials = this.loginForm.value;
+
+    this.onSubmit.emit({
+      email: credentials.email ?? '',
+      password: credentials.password ?? ''
+    })
+  }
+}
+
+type LoginValidationErrors = { 
+  email?: string,
+  password?: string
 }
