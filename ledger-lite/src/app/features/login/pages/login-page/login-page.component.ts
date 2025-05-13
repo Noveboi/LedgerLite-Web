34578b5577d694d1988b187c88b6ee7b62fefe18
Nove <cptgiorgos03@gmail.com>
2025-05-13 15:21:33 +0300

    Add LoginPage

--- ledger-lite/src/app/features/login/pages/login-page/login-page.component.ts
+++ ledger-lite/src/app/features/login/pages/login-page/login-page.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LoginFormComponent } from "../../components/login-form/login-form.component";
+import { LogoComponent } from "../../../../components/logo/logo.component";
+import { AuthService } from '../../../../core/services/auth-service';
+import { LoginRequest } from '../../types';
 
 @Component({
   selector: 'app-login-page',
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, LogoComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  private auth = inject(AuthService)
 
+  handleLogin(request: LoginRequest) {
+    this.auth.login(request)
+  }
 }
