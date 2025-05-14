20816eb89d742221473756b80da5867bd976df52
Nove <cptgiorgos03@gmail.com>
2025-05-14 11:50:20 +0300

    Add an auth HTTP client interceptor

---
+++ ledger-lite/src/app/core/interceptors/http.authInterceptor.ts
@@ -0,0 +1,18 @@
+import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { AuthService } from "../services/auth-service";
+import { inject } from "@angular/core";
+
+export const authInteceptor = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
+    const auth = inject(AuthService);
+    const token = auth.accessToken();
+    if (token === null)
+        return next(req);
+
+    console.log('Sending authenticated request')
+    const authReq = req.clone({
+        headers: req.headers.append('Authorization', `Bearer ${token}`)
+    })
+
+    return next(authReq);
+}
