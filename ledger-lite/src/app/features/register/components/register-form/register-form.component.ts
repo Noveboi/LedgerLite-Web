176b017f19ebe90f50c41cd2a8109d4c1649133b
George Nikolaidis <nove@novesoftware.net>
2025-05-14 09:16:21 +0300

    Start implementing RegisterPage

--- ledger-lite/src/app/features/register/components/register-form/register-form.component.ts
+++ ledger-lite/src/app/features/register/components/register-form/register-form.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormField, MatInputModule, MatButtonModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
-
+  registerForm = new FormGroup({
+    email: new FormControl('', [Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+    username: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl('')
+  }, (group): { [key: string]: any } | null => {
+    const password = group.get('password');
+    const confirm = group.get('confirmPassword');
+  })
 }
