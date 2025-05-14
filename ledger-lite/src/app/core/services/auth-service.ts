8fe9804012470fc4aa9ba454b124d981f06e63f8
George Nikolaidis <nove@novesoftware.net>
2025-05-14 08:25:54 +0300

    Intermediate commit

--- ledger-lite/src/app/core/services/auth-service.ts
+++ ledger-lite/src/app/core/services/auth-service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment as env } from '../../../environments/environment' ; 
 import { LoginRequest, LoginResponse } from '../../features/login/types';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private http = inject(HttpClient);
+  private auth$ = new BehaviorSubject<>
+
 
   login(request: LoginRequest): void {
     this.http.post<LoginResponse>(`${env.apiUrl}/login`, request).subscribe(response => {
