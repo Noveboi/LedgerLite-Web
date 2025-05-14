4d4ec6989662a644e91bb5ba4f3a6c0f898e1d6a
Nove <cptgiorgos03@gmail.com>
2025-05-14 08:18:27 +0300

    Adjust app.routes to use new anonymous layout

--- ledger-lite/src/app/app.routes.ts
+++ ledger-lite/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './features/login/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './features/register/pages/register-page/register-page.component';
+import { AnonymousLayoutComponent } from './layout/anonymous-layout/anonymous-layout.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginPageComponent }
+    { path: 'auth', component: AnonymousLayoutComponent, children: [
+        { path: 'login', component: LoginPageComponent },
+        { path: 'register', component: RegisterPageComponent }
+    ]}
 ];
