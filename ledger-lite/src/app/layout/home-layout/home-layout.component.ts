fb3c4314d772880e40601149f1f48d9414e5a9e3
George Nikolaidis <nove@novesoftware.net>
2025-05-17 14:21:51 +0300

    Improve home layout

--- ledger-lite/src/app/layout/home-layout/home-layout.component.ts
+++ ledger-lite/src/app/layout/home-layout/home-layout.component.ts
@@ -7,10 +7,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 import { AccountTreeComponent } from "../../features/accounts/components/account-tree/account-tree.component";
 import { LogoComponent } from "../../components/logo/logo.component";
+import { UserProfileBannerComponent } from "../../features/users/user-profile-banner/user-profile-banner.component";
 
 @Component({
   selector: 'app-home-layout',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, RouterOutlet, AccountTreeComponent, LogoComponent],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, RouterOutlet, AccountTreeComponent, LogoComponent, UserProfileBannerComponent],
   templateUrl: './home-layout.component.html',
   styleUrl: './home-layout.component.css'
 })
