d26e81f715d0642ebdc689090dfd80baeaafe61d
Nove <cptgiorgos03@gmail.com>
2025-05-20 21:43:50 +0300

    Add organization types/service

---
+++ ledger-lite/src/app/features/organizations/services/organization.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from '@angular/core';
+import { ApiService } from '../../../core/services/api/api.service';
+import { Observable } from 'rxjs';
+import { CreateOrganizationRequest, CreateOrganizationResponse } from '../organizations.types';
+import { AuthService } from '../../../core/services/auth-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrganizationService {
+  private api = inject(ApiService);
+  private auth = inject(AuthService);
+
+  create(request: CreateOrganizationRequest): void {
+    this.api.post<CreateOrganizationResponse>('/organizations', request).subscribe({
+      next: (resp) => {
+        this.auth.refresh()
+      }
+    });
+  }
+}
