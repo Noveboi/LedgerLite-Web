5cf52eb5d0247d4dc9d48aac17f6b7a1216f1b48
Nove <cptgiorgos03@gmail.com>
2025-05-20 19:56:34 +0300

    Add a conditional 'no-organization' page in home page

--- ledger-lite/src/app/features/home/home-page/home-page.component.ts
+++ ledger-lite/src/app/features/home/home-page/home-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth-service';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  private auth = inject(AuthService);
+  private router = inject(Router);
 
+  user = this.auth.user;
+
+  navigateToCreateOrganization() {
+    this.router.navigate(['organizations', 'create'])
+  }
 }
