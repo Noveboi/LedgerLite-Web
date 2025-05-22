8a73e8ec103f3c07c52ec6b12b4d71f1c695e8d1
Nove <cptgiorgos03@gmail.com>
2025-05-22 19:16:29 +0300

    Style 'Create Account' buttobn

--- ledger-lite/src/app/features/accounts/components/create-account-button/create-account-button.component.ts
+++ ledger-lite/src/app/features/accounts/components/create-account-button/create-account-button.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from "@angular/material/dialog"
+import { CreateAccountFormComponent } from '../create-account-form/create-account-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { CreateAccountDialogComponent } from '../create-account-dialog/create-account-dialog.component';
 
 @Component({
   selector: 'app-create-account-button',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './create-account-button.component.html',
   styleUrl: './create-account-button.component.css'
 })
 export class CreateAccountButtonComponent {
+  private dialog = inject(MatDialog)
 
+  openCreateAccountDialog() {
+    this.dialog.open(CreateAccountDialogComponent)
+  }
 }
