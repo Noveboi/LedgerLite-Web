becbee1163eb758dc5453c390b1398ed093877a3
Nove <cptgiorgos03@gmail.com>
2025-05-23 20:40:05 +0300

    Improve account tree styling

--- ledger-lite/src/app/features/accounts/components/account-tree/account-tree.component.ts
+++ ledger-lite/src/app/features/accounts/components/account-tree/account-tree.component.ts
@@ -1,17 +1,18 @@
-import { ChartAccountNode, ChartOfAccounts, SlimAccount } from '../../accounts.types';
-import {ChangeDetectionStrategy, Component, inject, input, TrackByFunction} from '@angular/core';
-import {MatTreeModule} from '@angular/material/tree';
+import { ChartAccountNode } from '../../accounts.types';
+import {Component, inject, TrackByFunction} from '@angular/core';
+import {MatTreeModule, MatTreeNodePadding} from '@angular/material/tree';
 import {MatIconModule} from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ChartOfAccountsService } from '../../services/chart-of-accounts.service';
 import { map } from 'rxjs';
 import { CreateAccountButtonComponent } from "../create-account-button/create-account-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-account-tree',
-  imports: [MatTreeModule, MatButtonModule, MatIconModule, CreateAccountButtonComponent],
+  imports: [MatTreeModule, MatButtonModule, MatIconModule, CreateAccountButtonComponent, RouterLink, MatTreeNodePadding],
   templateUrl: './account-tree.component.html',
-  styleUrl: './account-tree.component.css'
+  styleUrl: './account-tree.component.scss'
 })
 export class AccountTreeComponent {
   private accountService = inject(ChartOfAccountsService);
@@ -20,6 +21,8 @@ export class AccountTreeComponent {
   hasChild = (_: number, node: ChartAccountNode) => !!node.children && node.children.length > 0;
   trackByFn: TrackByFunction<ChartAccountNode> = (_: number, item: ChartAccountNode) => item.account.id;
   
+  indentPixels = 15;
+
   dataSource = this.accountService.chart$.pipe(
     map(chart => chart.accounts)
   )
