d4a912caae2de8ef21c0d27ec52179678d28bff3
George Nikolaidis <nove@novesoftware.net>
2025-05-24 21:28:11 +0300

    Change account navigation UX

--- ledger-lite/src/app/features/accounts/components/account-button-group/account-button-group.component.ts
+++ ledger-lite/src/app/features/accounts/components/account-button-group/account-button-group.component.ts
@@ -1,14 +1,13 @@
-import { Component, computed, HostListener, input, signal } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RemoveAccountButtonComponent } from "../remove-account-button/remove-account-button.component";
 import { CreateAccountButtonComponent } from "../create-account-button/create-account-button.component";
 import { SlimAccount } from '../../accounts.types';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-account-button-group',
-  imports: [RemoveAccountButtonComponent, CreateAccountButtonComponent, MatIconModule, MatButtonModule, RouterLink],
+  imports: [RemoveAccountButtonComponent, CreateAccountButtonComponent, MatIconModule, MatButtonModule],
   templateUrl: './account-button-group.component.html',
   styleUrl: './account-button-group.component.scss'
 })
--- ledger-lite/src/app/features/accounts/components/account-tree/account-tree.component.ts
+++ ledger-lite/src/app/features/accounts/components/account-tree/account-tree.component.ts
@@ -7,10 +7,11 @@ import { ChartOfAccountsService } from '../../services/chart-of-accounts.service
 import { map } from 'rxjs';
 import { CreateAccountButtonComponent } from "../create-account-button/create-account-button.component";
 import { AccountButtonGroupComponent } from "../account-button-group/account-button-group.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-account-tree',
-  imports: [MatTreeModule, MatButtonModule, MatIconModule, CreateAccountButtonComponent, MatTreeNodePadding, AccountButtonGroupComponent],
+  imports: [MatTreeModule, MatButtonModule, MatIconModule, CreateAccountButtonComponent, MatTreeNodePadding, AccountButtonGroupComponent, RouterLink],
   templateUrl: './account-tree.component.html',
   styleUrl: './account-tree.component.scss'
 })
@@ -20,7 +21,8 @@ export class AccountTreeComponent {
   childrenAccessor = (node: ChartAccountNode) => node.children ?? [];
   hasChild = (_: number, node: ChartAccountNode) => !!node.children && node.children.length > 0;
   trackByFn: TrackByFunction<ChartAccountNode> = (_: number, item: ChartAccountNode) => item.account.id;
-  
+  stopClickPropagation = (e: MouseEvent) => e.stopPropagation();
+   
   indentPixels = 15;
 
   dataSource = this.accountService.chart$.pipe(
