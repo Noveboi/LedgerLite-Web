ce34dbb4bf79af5f48a2062fb18727962d6a6798
George Nikolaidis <nove@novesoftware.net>
2025-05-24 18:47:52 +0300

    Implement token refreshing in HTTP interceptor

--- ledger-lite/src/app/core/interceptors/http.authInterceptor.ts
+++ ledger-lite/src/app/core/interceptors/http.authInterceptor.ts
@@ -1,19 +1,60 @@
-import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
+import { catchError, defer, EMPTY, filter, Observable, retry, retryWhen, switchMap, take, throwError, timeout, timer } from "rxjs";
 import { AuthService } from "../services/auth-service";
 import { inject } from "@angular/core";
 
 export const authInteceptor = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
-    const auth = inject(AuthService);
-    let token = auth.accessToken();
+  const auth = inject(AuthService);
 
-    if (token === null) {
-        return next(req);  
-    }
+  let token = auth.accessToken();
 
-    const authReq = req.clone({
-        headers: req.headers.append('Authorization', `Bearer ${token}`)
+  if (token && !req.url.includes('/refresh') && !req.url.includes('/login')) {
+    req = authenticateRequest(req, token);
+  }
+
+  return next(req).pipe(
+    catchError((error: unknown) => {
+      if (error instanceof HttpErrorResponse && error.status === 401 && !req.url.includes('/refresh')) {
+        return handleUnauthorized(req, next, auth);
+      }
+
+      return throwError(() => error);
     })
-
-    return next(authReq);
+  );
 }
+
+const handleUnauthorized = (req: HttpRequest<unknown>, next: HttpHandlerFn, auth: AuthService) => {
+  const addAuthorizationHeader = () => { 
+    const newToken = auth.accessToken();
+    if (newToken) {
+      return next(authenticateRequest(req, newToken))
+    }
+    return throwError(() => new Error('Token refresh failed.'))
+  } 
+  
+  if (auth.isRefreshing()) {
+    return waitForRefreshCompletion(auth).pipe(
+      switchMap(() => addAuthorizationHeader())
+    )
+  }
+
+  return auth.refresh().pipe(
+    switchMap(() => addAuthorizationHeader())
+  );
+}
+
+const waitForRefreshCompletion = (auth: AuthService) => {
+  return defer(() => {
+    return timer(0, 100).pipe(
+      filter(() => !auth.isRefreshing()),
+      take(1),
+      timeout(5000)
+    )
+  })
+}
+
+const authenticateRequest = (req: HttpRequest<unknown>, accessToken: string): HttpRequest<unknown> => {
+  return req.clone({
+    setHeaders: { Authorization: `Bearer ${accessToken}` }
+  })
+} 
