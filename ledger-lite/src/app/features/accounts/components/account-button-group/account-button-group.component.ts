080955d10e1de5ce88ad0c866e3bdd66b8568836
George Nikolaidis <nove@novesoftware.net>
2025-05-24 10:38:41 +0300

    Add link to account button group[

--- ledger-lite/src/app/features/accounts/components/account-button-group/account-button-group.component.ts
+++ ledger-lite/src/app/features/accounts/components/account-button-group/account-button-group.component.ts
@@ -2,13 +2,20 @@ import { Component, input } from '@angular/core';
 import { RemoveAccountButtonComponent } from "../remove-account-button/remove-account-button.component";
 import { CreateAccountButtonComponent } from "../create-account-button/create-account-button.component";
 import { SlimAccount } from '../../accounts.types';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-account-button-group',
-  imports: [RemoveAccountButtonComponent, CreateAccountButtonComponent],
+  imports: [RemoveAccountButtonComponent, CreateAccountButtonComponent, MatIconModule, MatButtonModule, RouterLink],
   templateUrl: './account-button-group.component.html',
   styleUrl: './account-button-group.component.css'
 })
 export class AccountButtonGroupComponent {
   account = input<SlimAccount>();
+
+  stopClickPropagation(e: MouseEvent) {
+    e.stopPropagation();
+  }
 }
