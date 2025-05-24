9a04119105cb45fc181573b3d8d6ef1de85c6c30
George Nikolaidis <nove@novesoftware.net>
2025-05-24 22:56:58 +0300

    Work on journal entry table

--- ledger-lite/src/app/features/journal-entries/journal-entry-table/journal-entry-table.component.ts
+++ ledger-lite/src/app/features/journal-entries/journal-entry-table/journal-entry-table.component.ts
@@ -1,15 +1,35 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { MatTableModule } from '@angular/material/table'
 import { JournalEntryLine } from '../journal-entry.types';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+
+interface JournalEntryTableRow {
+  occursAt: Date,
+  description: string,
+  transferAccount: string,
+  credit: number,
+  debit: number
+}
 
 @Component({
   selector: 'app-journal-entry-table',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatIcon, MatButton],
   templateUrl: './journal-entry-table.component.html',
-  styleUrl: './journal-entry-table.component.css'
+  styleUrl: './journal-entry-table.component.scss'
 })
 export class JournalEntryTableComponent {
   entries = input<readonly JournalEntryLine[]>();
 
-  columns = ['occursAt', 'entryDescription', 'credit', 'debit']
+  rows = computed<readonly JournalEntryTableRow[]>(() => {
+    return this.entries()?.map<JournalEntryTableRow>(entry => ({
+      credit: entry.credit,
+      debit: entry.debit,
+      description: entry.entryDescription,
+      occursAt: new Date(entry.occursAt),
+      transferAccount: 'Placeholder'
+    })) ?? [];
+  })
+
+  columns = ['occursAt', 'description', 'transferAccount', 'credit', 'debit']
 }
