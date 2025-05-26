c249d4a3f9b6f5bb72ed988e143f9da958978331
George Nikolaidis <nove@novesoftware.net>
2025-05-26 10:28:30 +0300

    Add fiscal period select in sidenav

--- ledger-lite/src/app/layout/home-layout/modes/normal/home-layout.component.ts
+++ ledger-lite/src/app/layout/home-layout/modes/normal/home-layout.component.ts
@@ -1,25 +1,28 @@
-import {Component, inject} from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { LogoComponent } from '../../../../components/logo/logo.component';
 import { SettingsService } from '../../../../core/services/storage/settings.service';
 import { SystemTheme } from '../../../../core/types/config.types';
 import { AccountTreeComponent } from '../../../../features/accounts/components/account-tree/account-tree.component';
 import { UserProfileBannerComponent } from '../../../../features/users/user-profile-banner/user-profile-banner.component';
+import { FiscalPeriodSelectComponent } from "../../../../features/fiscal-periods/components/fiscal-period-select/fiscal-period-select.component";
 
 @Component({
   selector: 'app-normal-home-layout',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, RouterOutlet, AccountTreeComponent, LogoComponent, UserProfileBannerComponent],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, RouterOutlet, AccountTreeComponent, LogoComponent, UserProfileBannerComponent, RouterLink, FiscalPeriodSelectComponent],
   templateUrl: './home-layout.component.html',
   styleUrl: './home-layout.component.css'
 })
 export class NormalHomeLayoutComponent {
   private settingsService = inject(SettingsService);
   
+  isDark = computed(() => this.settingsService.current().systemTheme === SystemTheme.Dark);
+
   toggleTheme() {
     this.settingsService.current.update(x => ({ 
       ...x, 
