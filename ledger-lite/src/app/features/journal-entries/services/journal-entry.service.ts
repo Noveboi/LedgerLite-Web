ea490c5baa97f629b19013be8b30d6707edcdf7b
Nove <cptgiorgos03@gmail.com>
2025-05-28 07:19:19 +0300

    Reference period service in jouranl entry service

--- ledger-lite/src/app/features/journal-entries/services/journal-entry.service.ts
+++ ledger-lite/src/app/features/journal-entries/services/journal-entry.service.ts
@@ -1,12 +1,14 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiService } from '../../../core/services/api/api.service';
 import { UpdateJournalEntryRequest, CreateJournalEntryRequest } from '../journal-entry.requests';
+import { FiscalPeriodService } from '../../fiscal-periods/services/fiscal-period.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JournalEntryService {
   private api = inject(ApiService);
+  private periodService = inject(FiscalPeriodService);
 
   updateJournalEntry(request: UpdateJournalEntryRequest) {
     console.log(request);
