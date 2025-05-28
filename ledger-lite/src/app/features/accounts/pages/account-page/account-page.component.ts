ee8599636e3edd0b08c38d2ea6047efb5b42a023
George Nikolaidis <nove@novesoftware.net>
2025-05-28 17:04:20 +0300

    Make account page react to fiscal period switch

--- ledger-lite/src/app/features/accounts/pages/account-page/account-page.component.ts
+++ ledger-lite/src/app/features/accounts/pages/account-page/account-page.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, Input } from '@angular/core';
 import { ChartOfAccountsService } from '../../services/chart-of-accounts.service';
 import { JournalEntryTableComponent } from "../../../journal-entries/components/journal-entry-table/journal-entry-table.component";
 import { AccountHeadingComponent } from "../../components/account-heading/account-heading.component";
+import { FiscalPeriodService } from '../../../fiscal-periods/services/fiscal-period.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-account-page',
@@ -11,7 +13,16 @@ import { AccountHeadingComponent } from "../../components/account-heading/accoun
 })
 export class AccountPageComponent {
   private accountService = inject(ChartOfAccountsService);
+  private periodService = inject(FiscalPeriodService);
   account = this.accountService.selectedAccount;
+
+  constructor() {
+    this.periodService.selectedPeriod$.pipe(takeUntilDestroyed()).subscribe(period => {
+      if (period) {
+        this.accountService.refreshSelectedAccount()
+      }
+    })
+  }
   
   @Input()
   set id(value: string) {
