923c8ee2f34e4efdca2cb7041c328306ce4065a5
Nove <cptgiorgos03@gmail.com>
2025-05-29 20:31:33 +0300

    Add HTTP PUT to ApiService

--- src/app/core/services/api/api.service.ts
+++ src/app/core/services/api/api.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
-import { catchError, map, Observable, of } from 'rxjs';
-import { ApiError } from '../../../types/error.types';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +16,10 @@ export class ApiService {
 
   post<TResponse>(route: string, body: any): Observable<TResponse> {
     return this.http.post<TResponse>(this.getUrl(route), body);
+  }
+
+  put<TResponse>(route: string, body?: any): Observable<TResponse> {
+    return this.http.put<TResponse>(this.getUrl(route), body);
   }
 
   delete<TResponse>(route: string): Observable<TResponse> {
