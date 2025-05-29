d914bb44587a43f64ec601e3f2319fb56ce45ab9
Nove <cptgiorgos03@gmail.com>
2025-05-29 17:10:26 +0300

    Use trial balance table in home page

--- src/app/features/home/home-page/home-page.component.ts
+++ src/app/features/home/home-page/home-page.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../core/services/auth-service';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { TrialBalanceTableComponent } from "../../reporting/trial-balance/components/trial-balance-table/trial-balance-table.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, TrialBalanceTableComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
